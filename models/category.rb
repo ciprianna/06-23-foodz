fff0af66728156ab49cbe7a27d6dce660c5f4097
Ciprianna <ciprianna17@gmail.com>
2015-06-24 17:48:34 -0500

    Finished working through some CRUD methods in the Category model.

--- models/category.rb
+++ models/category.rb
@@ -21,4 +21,57 @@ class Category
     @name = options["name"]
   end
 
+  # Adds a Category Object to the database
+  #
+  # Returns the Object if it was added to the database, or false if missing info
+  def add_to_database
+    if valid?
+      Category.add({"name" => "#{self.name}"})
+    else
+      false
+    end
+  end
+
+  # Returns all Food Objects in a Category
+  #
+  # category_id - Integer indicating the id of the category; foreign key in the
+  #               foods table
+  #
+  # Returns an Array of Food Objects
+  def foods
+    results = DATABASE.execute("SELECT * FROM foods WHERE category_id = '#{id}';")
+
+    store_results = []
+
+    results.each do |hash|
+      store_results << Food.new(hash)
+    end
+
+    return store_results
+  end
+
+  # Only deletes a Category Object if no Food Objects are assigned to it
+  #
+  # Returns true/false Boolean
+  def delete_category
+    if self.foods.empty?
+      self.delete
+    else
+      false
+    end
+  end
+
+  # Utility method to determine if an Object contains a valid field or not
+  #
+  # Returns true/false Boolean
+  def valid?
+    valid = true
+
+    if self.name.nil? || self.name == ""
+      valid = false
+    end
+
+    return valid
+  end
+
 end
