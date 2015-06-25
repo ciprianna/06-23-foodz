79a676075c0a7bcc392a31a607a1e6118725b3d0
Ciprianna <ciprianna17@gmail.com>
2015-06-25 10:52:55 -0500

    Finished working through CRUD operators for Recipe model. Mirrors   the Food model currently.

--- models/recipe.rb
+++ models/recipe.rb
@@ -42,4 +42,44 @@ class Recipe
     return store_results
   end
 
+  # Adds a new Object to the database if it has valid fields
+  #
+  # Returns the Object if it was added to the database or false if it failed
+  def add_to_database
+    if self.valid?
+      Recipe.add({"name" => "#{self.name}", "recipe_type" => "#{self.recipe_type}"})
+    else
+      false
+    end
+  end
+
+  # Ensures that an updated Recipe Object has a valid name and type before
+  #   saving
+  #
+  # Returns the Object if saved or false if save failed
+  def save_valid
+    if self.valid?
+      self.save
+    else
+      false
+    end
+  end
+
+  # Utility method to determine if an Object contains valid fields or not
+  #
+  # Returns true/false Boolean
+  def valid?
+    valid = true
+
+    if self.name.nil? || self.name == ""
+      valid = false
+    end
+
+    if self.recipe_type.nil? || self.recipe_type == ""
+      valid = false
+    end
+
+    return valid
+  end
+
 end
