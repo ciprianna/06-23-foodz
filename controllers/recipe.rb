570681b705d8dc69a269010c8a974da13d68502c
Ciprianna <ciprianna17@gmail.com>
2015-06-25 12:29:56 -0500

    Views for recipes by time are built.

--- controllers/recipe.rb
+++ controllers/recipe.rb
@@ -4,3 +4,25 @@
 get "/all_recipes" do
   erb :"recipes/all_recipes"
 end
+
+# ------------------------------------------------------------------------------
+# Displays recipes by recipe type
+# ------------------------------------------------------------------------------
+get "/recipes_by_type" do
+  erb :"recipes/recipes_by_type"
+end
+
+# ------------------------------------------------------------------------------
+# Displays recipes by time to make
+# ------------------------------------------------------------------------------
+# Step 1: Displays a form for the time category
+get "/recipes_by_time" do
+  erb :"recipes/recipes_by_time"
+end
+
+# Step 2: Save the form entries and display information back
+get "/display_recipes_by_time" do
+  @recipes_to_show = Recipe.where_time(params["recipes"]["time"])
+
+  erb :display_recipes_by_time
+end
