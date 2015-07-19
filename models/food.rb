2b26188f0f435f6f7ea840852c24b0d08495d3e4
Ciprianna <ciprianna17@gmail.com>
2015-07-19 17:08:29 -0500

    Cleared some older methods from the Food model.

--- models/food.rb
+++ models/food.rb
@@ -4,37 +4,6 @@ class Food < ActiveRecord::Base
 
   has_and_belongs_to_many :recipes
 
-  # # Gets food names
-  # #
-  # # list - Array of food_ids (Integer) from recipes_foods
-  # #
-  # # Returns an Array of food names (Strings)
-  # def self.get_names(list)
-  #   food_list = []
-  #   list.each do |food|
-  #     food = Food.find(food)
-  #     food_list << food.name
-  #   end
-  #   return food_list
-  # end
-
-  # # Matches selected foods to recipes in the recipes_foods table
-  # #
-  # # food_ids - Array of id's (Integers) for Food Objects
-  # #
-  # # Returns an Array of recipe_ids from the bridge table
-  # def self.match_foods(food_ids)
-  #   for_sql = food_ids.join(", ")
-  #   results = DATABASE.execute("SELECT * FROM recipes_foods WHERE food_id IN (#{for_sql});")
-  #
-  #   store_recipes = []
-  #   results.each do |hash|
-  #     store_recipes << hash["recipe_id"]
-  #   end
-  #
-  #   return store_recipes
-  # end
-
   # Counts the number of user ingredients that the recipe uses
   #
   # store_recipes - Array of recipe_ids (Integers) from recipes_foods
