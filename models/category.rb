021ceb27fa4cde5ba78baf0b92328bde457084a4
Ciprianna <ciprianna17@gmail.com>
2015-07-19 17:52:48 -0500

    Added a has_many relationship to the Category Model for foods.

--- models/category.rb
+++ models/category.rb
@@ -1,6 +1,8 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true
 
+  has_many :foods
+
   # Only deletes a Category Object if no Food Objects are assigned to it
   #
   # Returns true/false Boolean
